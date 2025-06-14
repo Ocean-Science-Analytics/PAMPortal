457ad8e0550d28d0ec3cb48024f6a94fc832cff5
Jsstephe <jaredstephens1999@gmail.com>
2025-06-13 23:19:08 -0700

    Updated Packages for deployment

---
+++ app.R
@@ -0,0 +1,6 @@
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp()
+library(pkgload)
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+options( "golem.app.prod" = TRUE)
+PAMPortal::run_app() # add parameters here (if any)
